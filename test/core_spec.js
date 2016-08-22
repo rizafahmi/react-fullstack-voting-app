5c28a0c177bc449034b80bd05f283cf19b238716
Riza Fahmi <rizafahmi@gmail.com>
2016-08-22 22:03:55 +0700

    Finish Voting mechanism

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,7 +1,7 @@
 import {List, Map} from 'immutable'
 import {expect} from 'chai'
 
-import {setEntries} from '../src/core'
+import {setEntries, next, vote} from '../src/core'
 
 describe('application logic', () => {
   describe('setEntries', () => {
@@ -23,4 +23,66 @@ describe('application logic', () => {
       }))
     })
   }) // end describe 'set entries'
+
+  describe('next', () => {
+    it('takes the next two entries under vote', () => {
+      const state = Map({
+        entries: List.of('Transpotting', '28 Days Later', 'Sunshine')
+      })
+
+      const nextState = next(state)
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Transpotting', '28 Days Later')
+        }),
+        entries: List.of('Sunshine')
+      }))
+    })
+  }) // end describe next
+
+  describe('vote', () => {
+    it('creates a tally for the for the voted entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Transpotting', '28 Days Later')
+        }),
+        entries: List()
+      })
+      const nextState = vote(state, 'Transpotting')
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Transpotting', '28 Days Later'),
+          tally: Map({
+            'Transpotting': 1
+          })
+        }),
+        entries: List()
+      }))
+    })
+    it('adds to existing tally for the voted entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Transpotting', '28 Days Later'),
+          tally: Map({
+            'Transpotting': 3,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List()
+      })
+
+      const nextState = vote(state, 'Transpotting')
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Transpotting', '28 Days Later'),
+          tally: Map({
+            'Transpotting': 4,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List()
+      }))
+    })
+
+  }) // end describe 'vote'
 }) // end describe 'application logic'
