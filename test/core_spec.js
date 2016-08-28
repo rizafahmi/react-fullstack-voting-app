85f1d11bd369f330d5da34af7081d7cd2490ee0b
Riza Fahmi <rizafahmi@gmail.com>
2016-08-28 18:55:12 +0700

    Finish voting mechanism

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,7 +1,7 @@
 import {List, Map} from 'immutable'
 import {expect} from 'chai'
 
-import {setEntries, next} from '../src/core'
+import {setEntries, next, vote} from '../src/core'
 
 describe('application logic', () => {
   describe('setEntries', () => {
@@ -37,4 +37,40 @@ describe('application logic', () => {
       }))
     })
   }) // end describe next
+
+  describe('vote', () => {
+    it('creates a tally for the voted entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        }),
+        entries: List()
+      })
+
+      const nextState = vote(state, 'Trainspotting')
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 1
+          })
+        }),
+        entries: List()
+      }))
+    }) // end it
+
+    it('adds to existing tally for the voted entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 4,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List()
+      })
+    })
+  })
+
 })
