2c286149d90660dab55832e18229c5da52dc5f48
Riza Fahmi & Rubi Henjaya <pair+rizafahmi.rubicode@devbootcamp.com>
2016-08-29 10:36:14 +0700

    Socket WIP

--- src/server.js
+++ src/server.js
@@ -7,5 +7,6 @@ export default function startServer() {
 
   io.on('connection', (socket) => {
     socket.emit('state', store.getState().toJS())
+    socket.on('action', store.dispatch.bind(store))
   })
 }
